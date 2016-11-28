4494b254a24c45f12ff8f9f7af68daec3f9b1a08
Matthew Ao <mattyayoh@gmail.com>
2016-11-28 00:20:23 -0500

    Side panel styling

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -45,12 +45,12 @@ function infoButtonPressed()
 }
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("theSidePanel").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("theSidePanel").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
 
