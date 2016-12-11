3e5964d04dac7371eb9dafc5f1150c782ede2f07
Matthew Ao <mattyayoh@gmail.com>
2016-12-11 00:00:32 -0500

    belay that

--- extension/js/mortality.js
+++ extension/js/mortality.js
@@ -2,6 +2,12 @@
 
   var $  = document.getElementById.bind(document);
 
+  var yearMS = 31556952000;
+  var monthMS = 2628000000;
+  var dayMS = 86400000;
+  var hourMS = 3600000;
+  var minuteMS = 60000;
+  var secondMS = 1000;
 
   var App = function(appElement)
   {
