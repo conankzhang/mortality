731151eea47f3e4126892975c11518b56982fc0a
Matthew Ao <mattyayoh@gmail.com>
2016-12-21 00:08:54 -0500

    Animate the timer shrink when side panel opens

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -47,11 +47,37 @@ function infoButtonPressed()
 function openNav() {
   document.getElementById("theSidePanel").style.width = "40vw";
   document.getElementById("main").style.marginLeft = "40vw";
+  $('.timer').animate({
+    'font-size':'3vw'
+  },1000);
+  $('.timer-container').animate({
+    'left':'70%'
+  },1000);
+
+  $('.timer-labels').animate({
+    'font-size':'0.8vw'
+  },1000);
+  $('.timer-labels').animate({
+    'margin-left':'-0.5vw'
+  },1000);
 }
 
 function closeNav() {
   document.getElementById("theSidePanel").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+  $('.timer').animate({
+    'font-size':'6vw'
+  },1000);
+  $('.timer-container').animate({
+    'left':'50%'
+  },1000);
+
+  $('.timer-labels').animate({
+    'font-size':'1.5vw'
+  },1000);
+  $('.timer-labels').animate({
+    'margin-left':'-1vw'
+  },1000);
 }
 
 $('#main').click(function() {
