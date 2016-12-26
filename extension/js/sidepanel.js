452635f15b9fe39bc1141169aa1bb8913c0fc0ee
Matt Ao <mattyayoh@gmail.com>
2016-12-26 12:03:51 -0500

    Start the migration over to side panel.js

---
+++ extension/js/sidepanel.js
@@ -0,0 +1,50 @@
+function openNav() {
+  document.getElementById("theSidePanel").style.width = "50vw";
+  document.getElementById("main").style.marginLeft = "50vw";
+  $('.timer').animate({
+    'font-size':'3vw'
+  },1000);
+  $('.timer-container').animate({
+    'left':'70%'
+  },1000);
+
+  $('.timer-labels').animate({
+    'font-size':'0.8vw'
+  },1000);
+  $('.timer-labels').animate({
+    'margin-left':'-0.5vw'
+  },1000);
+}
+
+function closeNav() {
+  document.getElementById("theSidePanel").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  $('.timer').animate({
+    'font-size':'6vw'
+  },1000);
+  $('.timer-container').animate({
+    'left':'50%'
+  },1000);
+
+  $('.timer-labels').animate({
+    'font-size':'1.5vw'
+  },1000);
+  $('.timer-labels').animate({
+    'margin-left':'-1vw'
+  },1000);
+}
+
+$('#main').click(function() {
+  closeNav();
+});
+
+$('#close-button').click(function()
+{
+  closeNav();
+});
+
+$('#info-button').click(function(e)
+{
+  openNav();
+  e.stopPropagation();
+});
