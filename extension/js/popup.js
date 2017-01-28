ca90c4eab9cf4e7c92c35b7ee8542c4f9146e1ff
Matt Ao <mattyayoh@gmail.com>
2017-01-27 20:44:08 -0500

    More housekeeping

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -22,100 +22,10 @@ String.prototype.format = function() {
 
 function setupCountdown()
 {
-  var specifyCountdownCheckbox = document.querySelector('input[id=specifyCountdown-checkbox]');
-  if( specifyCountdownCheckbox.checked )
-  {
-    loadCountdownDate();
-
-    var countdownTimeCheckbox = document.querySelector('input[id=countdown-addTime-checkbox]');
-    if( countdownTimeCheckbox.checked )
-    {
-      loadCountdownTime();
-    }
-  }
-  else
-  {
-    loadSurveyAnswers();
-  }
-
-  $("#countdown-submit-button").click(function(){
-    var toggleCountdownCheckbox = document.querySelector('input[id=toggleCountdown-checkbox]');
-
-    localStorage.setItem("countdownEnabled", "YES");
-    var specifyCountdownCheckbox = document.querySelector('input[id=specifyCountdown-checkbox]');
-    if( specifyCountdownCheckbox.checked )
-    {
-      var timeInput = document.getElementById("countdownTime-input").value;
-      var timeArray = timeInput.split(":");
-      localStorage.deathTime = timeArray[0]*60 + timeArray[1]*1;
-
-      saveCountdownDeath();
-
-      localStorage.setItem("specificTimeSet", "YES");
-      var countdownTimeCheckbox = document.querySelector('input[id=countdown-addTime-checkbox]');
-      if( countdownTimeCheckbox.checked )
-      {
-        localStorage.setItem("countdownTimeSet", "YES");
-      }
-      else
-      {
-        localStorage.removeItem("countdownTimeSet");
-      }
-      var countdownDailyCheckbox = document.querySelector('input[id=countdown-daily-checkbox]');
-      if( countdownDailyCheckbox.checked )
-      {
-        localStorage.setItem("countdownDaily", "YES");
-      }
-      else
-      {
-        localStorage.removeItem("countdownDaily");
-      }
-    }
-    else
-    {
-      localStorage.removeItem("specificTimeSet");
-      saveSurveyAnswers();
-    }
-
-    $("#info-popup").magnificPopup('close');
-  });
-
-  $("#countdown-cancel-button").click(function(){
-    $("#info-popup").magnificPopup('close');
-  });
-
-}
-
-function loadCountdownTime()
-{
-  var deathTime = localStorage.getItem("deathTime");
-  if( deathTime === null )
-  {
-    document.getElementById('countdownTime-input').value = "00:00";
-    localStorage.deathTime = "0";
-  }
-  else
-  {
-    document.getElementById('countdownTime-input').value = getTimeStringFromMinutes(deathTime);
-  }
-}
-
-function saveCountdownDeath()
-{
-  window.app.saveDeath();
-}
-
-function loadCountdownDate()
-{
-  var deathDate = localStorage.getItem("deathDate");
-  if( deathDate === null )
-  {
-    document.getElementById('countdownDate-input').value = new Date();
-  }
-  else
-  {
-    document.getElementById('countdownDate-input').value = new Date(parseInt(deathDate)).yyyymmdd();
-  }
+    var timeInput = document.getElementById("countdownTime-input").value;
+    var timeArray = timeInput.split(":");
+    localStorage.deathTime = timeArray[0]*60 + timeArray[1]*1;
+    window.app.saveDeath();
 }
 
 function setupThemes()
@@ -140,9 +50,6 @@ function setupSettings(dob, dobMinutes)
   // $("#submit-button").click(function(){
   //   window.app.saveDob();
   //   saveTheme();
-  //   savePrecision();
-  //   saveChapterLengths();
-  //   $("#info-popup").magnificPopup('close');
   // });
 }
 
