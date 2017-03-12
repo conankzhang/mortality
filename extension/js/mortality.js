f11a4929089ca82f5c1c583012f66eeab5876e2a
Matt Ao <mattyayoh@gmail.com>
2017-03-11 23:28:24 -0500

    Decimal strings

--- extension/js/mortality.js
+++ extension/js/mortality.js
@@ -378,6 +378,40 @@
       //Initial state before the timer for renderer kicks
       while(true)
       {
+        if( savedPrecision == "decimal" )
+        {
+          if( largestPrecision == "year" ) {
+            var years = endMoment.diff(originalStartMoment, 'years', true);
+            years = years.toFixed(3);
+            var yearString = zeroFill(years.toString(), 2);
+          }
+          else if( largestPrecision == "month" ) {
+            var months = endMoment.diff(originalStartMoment, 'months', true);
+            months = months.toFixed(3);
+            var monthString = zeroFill(months.toString(), 2);
+          }
+          else if( largestPrecision == "day" ) {
+            var days = endMoment.diff(originalStartMoment, 'days', true);
+            days = days.toFixed(3);
+            var dayString = zeroFill(days.toString(), 2);
+          }
+          else if( largestPrecision == "hour" ) {
+            var hours = endMoment.diff(originalStartMoment, 'hours', true);
+            hours = hours.toFixed(3);
+            var hourString = zeroFill(hours.toString(), 2);
+          }
+          else if( largestPrecision == "min" ) {
+            var minutes = endMoment.diff(originalStartMoment, 'minutes', true);
+            minutes = minutes.toFixed(3);
+            var minuteString = zeroFill(minutes.toString(), 2);
+          }
+          else if( largestPrecision == "sec" ) {
+            var seconds = endMoment.diff(originalStartMoment, 'seconds', true);
+            seconds = seconds.toFixed(3);
+            var secondString = zeroFill(seconds.toString(), 2);
+          }
+          break;
+        }
         var years = endMoment.diff(startMoment, 'years');
         startMoment.add(years, 'years');
         if( years < 0 ) years = 0;
